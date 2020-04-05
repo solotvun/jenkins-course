e7f7e029f597fe4db0c2dc072fd1a3826997abe1
solotvun <solotvun@mail.ru>
2020-04-04 21:33:05 -0500

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,9 +14,9 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('solotvun/nodejs')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('docker_hub_login')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
